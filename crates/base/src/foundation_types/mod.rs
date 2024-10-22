8905625239a067a50f6b3b766819a152041974ee
jhoeflaken <jhoeflaken@live.nl>
2024-10-22 07:46:46 +0200

    Finished integer64.

--- crates/base/src/foundation_types/mod.rs
+++ crates/base/src/foundation_types/mod.rs
@@ -1,27 +1,20 @@
 mod any;
-pub use any::Any;
-
 mod numeric;
-pub use numeric::Numeric;
-
 mod ordered;
-pub use ordered::Ordered;
-
 mod numeric_ordered;
-pub use numeric_ordered::OrderNumeric;
-
 mod integer;
-pub use integer::Integer;
-
 mod double;
-pub use double::Double;
-
 mod real;
-pub use real::Real;
 mod integer64;
+
+pub use any::Any;
+pub use numeric::Numeric;
+pub use ordered::Ordered;
+pub use numeric_ordered::OrderNumeric;
+pub use integer::Integer;
+pub use double::Double;
+pub use real::Real;
 pub use integer64::Integer64;
-
-
 
 
 
